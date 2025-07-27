bee8214cf527f9f49a8e86f70de86a778add554a
Sergei <teh.netaholic@gmail.com>
2025-07-27 10:33:57 +0200

    demo

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+using Avalonia.Animation;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            int counter = 0;
+            Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    counter++;
+                    Greeting = counter.ToString();
+                    await Task.Delay(1);
+                    OnPropertyChanged(nameof(Greeting));
+                }
+            });
+
+        }
+        public string Greeting { get; private set; } = "Welcome to Avalonia!";
+
+    }
+}
